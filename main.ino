eae3d5b32afabe10968a96e18f9a06dc97dc4942
Nícolas Zanatto <34211411+NicolasZanatto@users.noreply.github.com>
2022-04-29 19:38:02 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -2,12 +2,24 @@
 #define pinS1AMARELO 12
 #define pinS1VERDE 11
 
+#define pinS2VERMELHO 10
+#define pinS2AMARELO 9
+#define pinS2VERDE 8
+
 #define pinS3VERMELHO 7
 #define pinS3AMARELO 6
 #define pinS3VERDE 5
 
 #define pinS4VERMELHO 4
 #define pinS4VERDE 3
+
+#define pinS5VERMELHO A0
+#define pinS5VERDE A1
+
+#define pinS6VERMELHO A2
+#define pinS6VERDE A3
+
+#define pinBot A4
 
 const int S1 = 0;
 const int S2eS3eS4 = 1;
@@ -25,12 +37,16 @@ void trocarEstado(int novoEstado){
 
 
 void pedestreLiberado(){
+  pinSEMAFOROS5(pinS5VERDE);
+  pinSEMAFOROS6(pinS6VERDE);
   Serial.println("S5 E S6 VERDE");
 }
 
 void pedestreBloquado(){
   if(botaoPedestrePressionado){
     Serial.println("S5 E S6 VERMELHO");
+    pinSEMAFOROS5(pinS5VERMELHO);
+    pinSEMAFOROS6(pinS6VERMELHO);
     botaoPedestrePressionado = 0;
   }
 }
@@ -39,6 +55,13 @@ void pinSEMAFOROS1(int pinMostrar){
   digitalWrite(pinS1VERMELHO,0);
   digitalWrite(pinS1AMARELO,0);
   digitalWrite(pinS1VERDE,0);
+  digitalWrite(pinMostrar,1);
+}
+
+void pinSEMAFOROS2(int pinMostrar){
+  digitalWrite(pinS2VERMELHO,0);
+  digitalWrite(pinS2AMARELO,0);
+  digitalWrite(pinS2VERDE,0);
   digitalWrite(pinMostrar,1);
 }
 
@@ -55,25 +78,49 @@ void pinSEMAFOROS4(int pinMostrar){
   digitalWrite(pinMostrar,1);
 }
 
+void pinSEMAFOROS5(int pinMostrar){
+  digitalWrite(pinS5VERMELHO,0);
+  digitalWrite(pinS5VERDE,0);
+  digitalWrite(pinMostrar,1);
+}
+
+void pinSEMAFOROS6(int pinMostrar){
+  digitalWrite(pinS6VERMELHO,0);
+  digitalWrite(pinS6VERDE,0);
+  digitalWrite(pinMostrar,1);
+}
+
 void setup()
 {
+  pinMode(pinBot, INPUT_PULLUP);
   //PINS 
   pinMode(pinS1VERMELHO, OUTPUT);
   pinMode(pinS1AMARELO, OUTPUT);
   pinMode(pinS1VERDE, OUTPUT);
+  pinMode(pinS2VERMELHO, OUTPUT);
+  pinMode(pinS2AMARELO, OUTPUT);
+  pinMode(pinS2VERDE, OUTPUT);
   pinMode(pinS3VERMELHO, OUTPUT);
   pinMode(pinS3AMARELO, OUTPUT);
   pinMode(pinS3VERDE, OUTPUT);
   pinMode(pinS4VERMELHO, OUTPUT);
   pinMode(pinS4VERDE, OUTPUT);
+  digitalWrite(pinS2VERMELHO,1);
   digitalWrite(pinS3VERMELHO,1);
   digitalWrite(pinS4VERMELHO,1);
+  digitalWrite(pinS5VERMELHO,1);
+  digitalWrite(pinS6VERMELHO,1);
 
   Serial.begin(9600);
   tempo=millis();
 }
 void loop()
 {
+
+  if(digitalRead(pinBot)==LOW){
+    botaoPedestrePressionado = 1;
+  }
+  
   if (estado==S1)
   {
     if(millis()-tempo==1000){
@@ -95,6 +142,7 @@ void loop()
   {
     if(millis()-tempo==1000){
       Serial.println("S2, S3 e S4 VERDE");
+      pinSEMAFOROS2(pinS2VERDE);
       pinSEMAFOROS3(pinS3VERDE);
       pinSEMAFOROS4(pinS4VERDE);
     }
@@ -107,10 +155,12 @@ void loop()
     }
     if(millis()-tempo==12000){
       Serial.println("S2 E S3 AMARELO");
+      pinSEMAFOROS2(pinS2AMARELO);
       pinSEMAFOROS3(pinS3AMARELO);
     }
     if(millis()-tempo == 15000){
       Serial.println("S2, S3, S4 VERMELHO");
+      pinSEMAFOROS2(pinS2VERMELHO);
       pinSEMAFOROS3(pinS3VERMELHO);
       pinSEMAFOROS4(pinS4VERMELHO);
       pedestreBloquado();
@@ -118,8 +168,3 @@ void loop()
     }       
   }
 }
-
-
-
-
-
